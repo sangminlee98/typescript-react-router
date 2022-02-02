6431abc324fda6a8b2d61c83a689123cdbd96399
sangminlee98 <tkdals0975@naver.com>
2022-02-02 20:00:17 +0900

    use sub router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Link, Route } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
-import Profile from './components/Profile';
+import Profiles from './components/Profiles';
 
 const App = () => {
   return (
@@ -10,12 +10,11 @@ const App = () => {
       <ul>
         <li><Link to='/'>홈</Link></li>
         <li><Link to='/about'>소개</Link></li>
-        <li><Link to='/profile/velopert'>velopert 프로필</Link></li>
-        <li><Link to='/profile/gildong'>gildong 프로필</Link></li>
+        <li><Link to='/profiles/'>프로필</Link></li>
       </ul>
       <Route path="/" exact component={Home}/>
       <Route path={['/about', '/info']} component={About}/>
-      <Route path='/profile/:username' component={Profile}/>
+      <Route path='/profiles' component={Profiles}/>
     </div>
   );
 }
