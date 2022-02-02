fa93968fedf804d5ccb17499587758957fb49c07
sangminlee98 <tkdals0975@naver.com>
2022-02-02 20:56:53 +0900

    withRouter

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
+import WithRouterSample from './WithRouterSample';
 
 type Data = {
   velopert: {name: string, description: string},
@@ -28,6 +29,7 @@ const Profile = ({match}: RouteComponentProps<MatchParams>) => {
         {username}({profile.name})
       </h3>
       <p>{profile.description}</p>
+      <WithRouterSample/>
     </div>
   );
 };
