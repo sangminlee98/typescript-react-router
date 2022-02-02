7279868dbaacbd699ccfaf9bcbff54b29df9e422
sangminlee98 <tkdals0975@naver.com>
2022-02-02 18:39:53 +0900

    url parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li><Link to='/'>홈</Link></li>
+        <li><Link to='/about'>소개</Link></li>
+        <li><Link to='/profile/velopert'>velopert 프로필</Link></li>
+        <li><Link to='/profile/gildong'>gildong 프로필</Link></li>
+      </ul>
+      <Route path="/" exact component={Home}/>
+      <Route path={['/about', '/info']} component={About}/>
+      <Route path='/profile/:username' component={Profile}/>
     </div>
   );
 }
-
 export default App;
