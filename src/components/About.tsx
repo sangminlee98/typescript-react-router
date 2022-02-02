01241edbbb04f36c232c107edcc1a8ae1ec1c70f
sangminlee98 <tkdals0975@naver.com>
2022-02-02 19:50:52 +0900

    use url query

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import qs from 'qs';
+import { RouteComponentProps } from 'react-router';
 
-const About = () => {
+type Location = {
+  detail: string
+}
+const About = ({ location }: RouteComponentProps<Location>) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  const showDetail = query.detail === 'true';
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
     </div>
   );
 };
