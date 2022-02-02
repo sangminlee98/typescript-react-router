c6a5c8be258b9c4a5b1ca70d0fc485b0895d20ba
sangminlee98 <tkdals0975@naver.com>
2022-02-02 21:06:36 +0900

    use Switch & NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import HistorySample from './components/HistorySample';
@@ -14,10 +14,21 @@ const App = () => {
         <li><Link to='/profiles/'>프로필</Link></li>
         <li><Link to='/history'>History 예제</Link></li>
       </ul>
-      <Route path="/" exact component={Home}/>
-      <Route path={['/about', '/info']} component={About}/>
-      <Route path='/profiles' component={Profiles}/>
-      <Route path='/history' component={HistorySample}/>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path={['/about', '/info']} component={About}/>
+        <Route path='/profiles' component={Profiles}/>
+        <Route path='/history' component={HistorySample}/>
+        <Route
+          render={({location}: RouteComponentProps) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}  
+        />
+      </Switch>
     </div>
   );
 }
--- src/components/Profiles.tsx
+++ src/components/Profiles.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+  const activeStyle = {
+    background: 'black',
+    color: 'white'
+  }
   return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
-        <li><Link to='/profiles/velopert'>velopert</Link></li>
-        <li><Link to='/profiles/gildong'>gildong</Link></li>
+        <li><NavLink activeStyle={activeStyle} to='/profiles/velopert'>velopert</NavLink></li>
+        <li><NavLink activeStyle={activeStyle} to='/profiles/gildong'>gildong</NavLink></li>
       </ul>
       
       <Route
